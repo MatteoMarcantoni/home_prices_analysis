56df3dbbf4c4b282e1cb077a7f69ddde930b5745
Matteo Marcantoni <matteomarcantoni1993@gmail.com>
2025-07-24 15:52:14 +0200

    studying the cbsodata code and trying to understand its functioning

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from APIClient import get_odata
+import pandas as pd
+import APIClient
 
 def main():
-    target_url = "https://beta-odata4.cbs.nl/CBS/83765NED"
-
-    # Downloaden van gehele tabel, duurt ongeveer 2 minuten
-    target_url = target_url + "/Observations"
-    # Downloaden van eerste 100 rijen uit de tabel
-
-    # target_url = target_url + "/Observations?$top=100"
-    data = get_odata(target_url)
+    identifier = '83625NED'
+    # Downloaden van gehele tabel (kan een halve minuut duren)
+    data = pd.DataFrame(APIClient.get_data(identifier))
     print(data.head())
     
 if __name__ == "__main__":
